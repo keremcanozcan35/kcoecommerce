9f593a6edfbbb56ba9d59f026df52b92fc6774ed
keremcanozcan35 <keremcanozcan35@gmail.com>
2025-10-26 01:28:52 +0300

    Revert "sadas"
    This reverts commit aa8797a5cdc5822b49a7840e15a3b241c0336a92.

--- src/main/java/com/ecommerce/kco_ecom/Controller/CategoryController.java
+++ src/main/java/com/ecommerce/kco_ecom/Controller/CategoryController.java
@@ -18,8 +18,6 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CategoryController.class);
-
     @GetMapping("/public/categories")
     public ResponseEntity<List<Category>> getAllCategories() {
         try {
